5663b6e1b81d8e4e2fd6ae7021eb707952e98895
vdhuyy <23021408@vnu.edu.vn>
2025-03-07 14:23:54 +0700

    Add files via upload

---
+++ 23021408_Lect3_Assignments/bai1.cpp
@@ -0,0 +1,7 @@
+//Sắp xếp theo mức độ tăng dần Big O
+//1. O(1): 2^10
+//2. O(n): 4logn+2n, 3n+100logn, 4n, 2^(logn)
+//3. O(n logn): nlogn
+//4. O(n^2): n^2 + 10n
+//5. O(n^3): n^3
+//6. O(2^n): 2^n
